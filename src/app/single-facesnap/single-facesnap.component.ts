ce946c8183e8ff391badd0b2f15f90b05e475de3
Madsen Servius <madsen.servius@loto-lakay.com>
2022-08-31 15:17:01 -0400

    add routing

---
+++ src/app/single-facesnap/single-facesnap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-facesnap',
+  templateUrl: './single-facesnap.component.html',
+  styleUrls: ['./single-facesnap.component.scss']
+})
+export class SingleFacesnapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
